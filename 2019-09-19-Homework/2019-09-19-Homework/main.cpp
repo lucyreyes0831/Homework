3e936511b90cfd38f25250f19b3d32e1e1078f79
lucyreyes0831 <55443270+lucyreyes0831@users.noreply.github.com>
2019-09-19 08:39:31 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,22 @@
+// Name: Lucy Reyes
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream> 
+
+int main()
+{
+	std::cout << "Name: Lucy Reyes" <<std::endl 
+		      << "Period 1: Intro to Computer Science" <<std::endl
+		      << "Period 2: Global History and Geography 1" <<std::endl
+		      << "Period 3: French 1" <<std::endl
+		      << "Period 4: English 10" <<std::endl
+		      << "Period 5: Lunch" <<std::endl
+		      << "Period 6: Algebra 2 CC" <<std::endl
+		      << "Period 7: Health" <<std::endl
+		      << "Period 8: Forensics" <<std::endl
+		      << "Period 9: House" <<std::endl
+		      << "Period 10: Physical Education" <<std::endl
+} 
